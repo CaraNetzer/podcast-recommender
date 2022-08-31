8e89b8253d316ededc311508021910c00773ab8a
Cara Netzer <cara.netzer@gmail.com>
2022-08-31 10:42:04 -0400

    finished webgit add .git add . wooooo

--- src/recommender/RecommendationWeb.js
+++ src/recommender/RecommendationWeb.js
@@ -4,91 +4,121 @@ export const RecommendationWeb = ({ access_token }) => {
     const [allHosts, getAllHosts] = useState([])
     const [numOfHosts, setNumOfHosts] = useState(0)
     const [selectedHost, setSelectedHost] = useState("")
+    const recommendedEpisodes = []
 
-    useEffect(
-        () => {
-            fetch('http://localhost:8088/favoriteHosts')
-                .then(response => response.json())
-                .then(hosts => {
-                    getAllHosts(hosts)
-                    setNumOfHosts(hosts.length)
-                })
-        }, [])
+    for (const host of allHosts) {
+        const searchQuery = `${selectedHost.name} and ${host.name}`
 
-    let leftHosts = []
-    let rightHosts = []
-
-    const splitHosts = (host) => {
-        const copy = [...allHosts]
-        const i = copy.indexOf(host)
-        copy.splice(i, 1)
-        if (numOfHosts % 2 === 0) {
-            leftHosts = copy[0, (numOfHosts / 2) - 1]
-            rightHosts = copy[(numOfHosts / 2) - 1, numOfHosts - 1]
-        } else {
-            leftHosts = copy.slice(0, Math.floor(numOfHosts / 2))
-            rightHosts = copy.slice(Math.floor(numOfHosts / 2), numOfHosts + 1)
+        const searchEpisodesUrl = new URL("https://api.spotify.com/v1/search")
+        const searchEpisodeParams = {
+            q: searchQuery,
+            type: "episode"
         }
+        searchEpisodesUrl.search = new URLSearchParams(searchEpisodeParams).toString();
+
+
+        fetch(searchEpisodesUrl, {
+            headers: {
+                Authorization: `Bearer ${access_token}`
+            }
+        })
+            .then(r => r.json())
+            .then(data => {
+                recommendedEpisodes.push({
+                    host: host.name,
+                    episodes: data.episodes?.items
+                })
+                //console.log(recommendedEpisodes)
+            })
     }
 
-    const selectHost = (selectedHost) => {
-        setSelectedHost(selectedHost)
 
-        splitHosts(selectedHost)
+useEffect(
+    () => {
+        fetch('http://localhost:8088/favoriteHosts')
+            .then(response => response.json())
+            .then(hosts => {
+                getAllHosts(hosts)
+                setNumOfHosts(hosts.length)
+            })
+    }, [])
 
-        const leftHTML = leftHosts.map(host => `<div class="sideWebCircle">
+let leftHosts = []
+let rightHosts = []
+
+const splitHosts = (host) => {
+    const copy = [...allHosts]
+    const i = copy.indexOf(host)
+    copy.splice(i, 1)
+    if (numOfHosts % 2 === 0) {
+        leftHosts = copy.slice(0, (numOfHosts / 2))
+        console.log(leftHosts)
+        rightHosts = copy.slice((numOfHosts / 2), numOfHosts - 1)
+        console.log(rightHosts)
+    } else {
+        leftHosts = copy.slice(0, Math.floor(numOfHosts / 2))
+        rightHosts = copy.slice(Math.floor(numOfHosts / 2), numOfHosts + 1)
+    }
+}
+
+const selectHost = (selectedHost) => {
+    setSelectedHost(selectedHost)
+
+    splitHosts(selectedHost)
+
+    const leftHTML = leftHosts.map(host => `<div class="sideWebCircle">
             <h3 class="other-host-name">${host.name}</h3>
             <div class="web-results">
                 <h5 class="rec-heading">Recommended Episodes for: ${host.name} and ${selectedHost.name}</h5>
                 <div class="rec-episodes">
-                    <article class="rec-episode"><img src="#" alt="#" /></article>
-                    <article class="rec-episode"><img src="#" alt="#" /></article>
-                    <article class="rec-episode"><img src="#" alt="#" /></article>
+                    <a href="https://open.spotify.com/episode/${recommendedEpisodes.find(object => object.host === host.name)?.episodes[0].id}" class="rec-episode" target="_blank"><img src="${recommendedEpisodes.find(object => object.host === host.name)?.episodes[0].images[0].url}" alt="#" /></a>
+                    <a href="https://open.spotify.com/episode/${recommendedEpisodes.find(object => object.host === host.name)?.episodes[1].id}" class="rec-episode" target="_blank"><img src="${recommendedEpisodes.find(object => object.host === host.name)?.episodes[1].images[0].url}" alt="#" /></a>
+                    <a href="https://open.spotify.com/episode/${recommendedEpisodes.find(object => object.host === host.name)?.episodes[2].id}" class="rec-episode" target="_blank"><img src="${recommendedEpisodes.find(object => object.host === host.name)?.episodes[2].images[0].url}" alt="#" /></a>
                 </div>    
-            </div>
+                </div>
         </div>`)
 
-        const rightHTML = rightHosts.map(host => `<div class="sideWebCircle">
+    const rightHTML = rightHosts.map(host => `<div class="sideWebCircle">
             <h3 class="other-host-name">${host.name}</h3>
             <div class="web-results">
                 <h5 class="rec-heading">Recommended Episodes for: ${host.name} and ${selectedHost.name}</h5>
                 <div class="rec-episodes">
-                    <article class="rec-episode"><img src="#" alt="#" /></article>
-                    <article class="rec-episode"><img src="#" alt="#" /></article>
-                    <article class="rec-episode"><img src="#" alt="#" /></article>
-                </div>
+                    <a href="https://open.spotify.com/episode/${recommendedEpisodes.find(object => object.host === host.name)?.episodes[0].id}" class="rec-episode" target="_blank"><img src="${recommendedEpisodes.find(object => object.host === host.name)?.episodes[0].images[0].url}" alt="#" /></a>
+                    <a href="https://open.spotify.com/episode/${recommendedEpisodes.find(object => object.host === host.name)?.episodes[1].id}" class="rec-episode" target="_blank"><img src="${recommendedEpisodes.find(object => object.host === host.name)?.episodes[1].images[0].url}" alt="#" /></a>
+                    <a href="https://open.spotify.com/episode/${recommendedEpisodes.find(object => object.host === host.name)?.episodes[2].id}" class="rec-episode" target="_blank"><img src="${recommendedEpisodes.find(object => object.host === host.name)?.episodes[2].images[0].url}" alt="#" /></a>
+                </div> 
             </div>
         </div>`)
 
-        document.querySelector(".middleCircle").innerHTML = `<div id="middleWebCircle">
+    document.querySelector(".middleCircle").innerHTML = `<div id="middleWebCircle">
                 <h3 id="selected-host-name">${selectedHost.name}</h3>
             </div>`
-        document.querySelector(".left-side").innerHTML = leftHTML.join("")
-        document.querySelector(".right-side").innerHTML = rightHTML.join("")
-    }
+    document.querySelector(".left-side").innerHTML = leftHTML.join("")
+    document.querySelector(".right-side").innerHTML = rightHTML.join("")
+}
 
-    return <>
-        <nav className="navbar navbar-light">
-            {allHosts?.map(host =>
-                <a key={host.id} className="nav-link" onClick={(e) => {
-                    e.preventDefault()
-                    selectHost(host)
-                }} href="#">{host.name}</a>
-            )}
-        </nav>
-        <div className="web">
-            <div className="left-side">
+return <>
+    <nav className="navbar navbar-light">
+        {allHosts?.map(host =>
+            <a key={host.id} className="my-nav-link" onClick={(e) => {
+                e.preventDefault()
+                selectHost(host)
+            }} href="#">{host.name}</a>
+        )}
+    </nav>
+    <div className="web">
+        <div className="left-side">
 
-            </div>
-            <div className="middleCircle">
-
-            </div>
-            <div className="right-side">
-
-            </div>
         </div>
-        {selectedHost ? ""
-            : <h3 className="pre-text">Select a host above</h3>
-        }
-    </>
+        <div className="middleCircle">
+
+        </div>
+        <div className="right-side">
+
+        </div>
+    </div>
+    {selectedHost ? ""
+        : <h3 className="pre-text">Select a host above</h3>
+    }
+</>
 }
